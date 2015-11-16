e28f2371355ed2e5e23a5609ea5b1700f25e8320
redroy44 <piobandu@student.pg.gda.pl>
2015-11-16 09:20:01 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@ unsigned int order = 2;
 for(unsigned int i = order; i < signal.n_rows; i++) {
    signal(i) = 1.02*signal(i-1) - 0.53*signal(i-2) + as_scalar(randn(1));
 }
-signal.print();
+//signal.print();
 //signal.transform([](double val) {return (0.3*sin(val)+0.7*cos(val)+0.5*as_scalar(randu(1)));});
 //signal.print();
 
