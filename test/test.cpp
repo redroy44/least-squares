fa9f1ce547e02d6fc6c8723374cadeb8eeed21de
Redroy44 <redroy44@gmail.com>
2015-11-23 22:49:08 +0100

    build type release

--- test/test.cpp
+++ test/test.cpp
@@ -15,7 +15,7 @@ TEST(LeastSquares, Offline) {
    unsigned int length = 1000;
    vec signal = ones(length + order);
    ASSERT_TRUE(signal.load("../test/data/signal.dat", raw_ascii));
-   ASSERT_EQ(1000, signal.n_rows);
+   ASSERT_EQ(1000u, signal.n_rows);
    ls::LeastSquares obj = ls::LeastSquares(signal, order);
    EXPECT_NO_THROW(obj.estimate());
    vec theta_est = obj.getTheta();
@@ -29,7 +29,7 @@ TEST(LeastSquares, Online) {
    unsigned int length = 1000;
    vec signal = ones(length + order);
    ASSERT_TRUE(signal.load("../test/data/signal.dat", raw_ascii));
-   ASSERT_EQ(1000, signal.n_rows);
+   ASSERT_EQ(1000u, signal.n_rows);
    ls::LeastSquares obj = ls::LeastSquares(order);
 
    boost::circular_buffer<double> cb(order);
@@ -51,7 +51,7 @@ TEST(LeastSquares, Online) {
    for(unsigned int i = order; i < signal_est.n_rows; i++) {
       signal_est(i) = theta_est(0)*signal_est(i-1) + theta_est(1)*signal_est(i-2) + sqrt(obj.getNoiseVar())*as_scalar(randn(1));
    }
-   ASSERT_EQ(1000, signal_est.n_rows);
+   ASSERT_EQ(1000u, signal_est.n_rows);
    ASSERT_TRUE(signal_est.save("signal_est_LS.dat", raw_ascii));
 }
 
